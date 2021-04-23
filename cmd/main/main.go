8ab17c4049e2e194202a9c76cde953b4ce7fc6d0
Sadi <sadi.toirov2003@gmail.com>
2021-04-24 01:12:43 +0500

    just for checking:)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ToirovSadi/wallet/pkg/wallet"
 	"log"
 )
@@ -8,39 +9,60 @@ import (
 func main() {
 	s := wallet.Service{}
 
-	account, err := s.RegisterAccount("+9921231234")
-	if err != nil{
+	err := s.Import("./data")
+	if err != nil {
 		log.Print(err)
 		return
 	}
 
-	err = s.Deposit(account.ID, 1000000)
-	if err != nil{
-		log.Print(err)
-		return
+	fmt.Println("accounts are:")
+	for _, account := range s.GetAccounts() {
+		fmt.Printf("%+v\n", account)
 	}
 
-	for i := 0; i < 15; i ++{
-		payment, err := s.Pay(account.ID, 5, "hichi")
+	//fmt.Println("payments are:")
+	//for _, payment := range s.GetPayments(){
+	//	fmt.Printf("%+v\n", payment)
+	//}
 
-		if err != nil{
-			log.Print(err)
-			return
-		}
-		if i % 3 == 0{
-			_, err = s.FavoritePayment(payment.ID, "nothing")
-			if err != nil{
-				log.Print(err)
-				return
-			}
-		}
-	}
+	//fmt.Println("favorites are:")
+	//for _, favorite := range s.GetFavorites(){
+	//	fmt.Printf("%+v\n", favorite)
+	//}
 
-	err = s.Export("./data")
-	if err != nil{
-		log.Print(err)
-		return
-	}
+	//account, err := s.RegisterAccount("+9921231234")
+	//if err != nil{
+	//	log.Print(err)
+	//	return
+	//}
+	//
+	//err = s.Deposit(account.ID, 1000000)
+	//if err != nil{
+	//	log.Print(err)
+	//	return
+	//}
+	//
+	//for i := 0; i < 15; i ++{
+	//	payment, err := s.Pay(account.ID, 5, "hichi")
+	//
+	//	if err != nil{
+	//		log.Print(err)
+	//		return
+	//	}
+	//	if i % 3 == 0{
+	//		_, err = s.FavoritePayment(payment.ID, "nothing")
+	//		if err != nil{
+	//			log.Print(err)
+	//			return
+	//		}
+	//	}
+	//}
+	//
+	//err = s.Export("./data")
+	//if err != nil{
+	//	log.Print(err)
+	//	return
+	//}
 
 	log.Print("Done!!!")
 }
