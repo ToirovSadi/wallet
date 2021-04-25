634dd639266b3ae60da2d8d8885432ecd91c140b
Sadi <sadi.toirov2003@gmail.com>
2021-04-26 01:59:05 +0500

    fix cover

--- pkg/wallet/export_test.go
+++ pkg/wallet/export_test.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"github.com/ToirovSadi/wallet/pkg/types"
+	"reflect"
 	"testing"
 )
 
@@ -103,6 +104,109 @@ func TestService_ExportToFile(t *testing.T) {
 			}
 			if err := s.ExportToFile(tt.args.path); (err != nil) != tt.wantErr {
 				t.Errorf("ExportToFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestService_ExportAccountHistory(t *testing.T) {
+	type fields struct {
+		nextAccountID int64
+		accounts      []*types.Account
+		payments      []*types.Payment
+		favorites     []*types.Favorite
+	}
+	type args struct {
+		accountID int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []*types.Payment
+		wantErr bool
+	}{
+		{
+			name: "test1",
+			fields: fields{
+				nextAccountID: 4,
+				accounts:      accounts,
+				payments:      payments,
+				favorites:     favorites,
+			},
+			args: args{
+				accountID: 1,
+			},
+			want: []*types.Payment{
+				payments[0],
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				nextAccountID: tt.fields.nextAccountID,
+				accounts:      tt.fields.accounts,
+				payments:      tt.fields.payments,
+				favorites:     tt.fields.favorites,
+			}
+			got, err := s.ExportAccountHistory(tt.args.accountID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ExportAccountHistory() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExportAccountHistory() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestService_HistoryToFiles(t *testing.T) {
+	type fields struct {
+		nextAccountID int64
+		accounts      []*types.Account
+		payments      []*types.Payment
+		favorites     []*types.Favorite
+	}
+	type args struct {
+		payments []*types.Payment
+		dir      string
+		records  int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test1",
+			fields: fields{
+				nextAccountID: 4,
+				accounts:      accounts,
+				payments:      payments,
+				favorites:     favorites,
+			},
+			args: args{
+				payments: payments,
+				dir:      ".",
+				records:  2,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Service{
+				nextAccountID: tt.fields.nextAccountID,
+				accounts:      tt.fields.accounts,
+				payments:      tt.fields.payments,
+				favorites:     tt.fields.favorites,
+			}
+			if err := s.HistoryToFiles(tt.args.payments, tt.args.dir, tt.args.records); (err != nil) != tt.wantErr {
+				t.Errorf("HistoryToFiles() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
