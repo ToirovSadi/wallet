1c3f13292ebedaa72dba3632d81175b7380b54f4
Sadi <sadi.toirov2003@gmail.com>
2021-04-26 01:28:48 +0500

    fix cover

--- pkg/wallet/export_test.go
+++ pkg/wallet/export_test.go
@@ -30,7 +30,7 @@ func TestService_Export(t *testing.T) {
 				favorites:     favorites,
 			},
 			args: args{
-				dir: "../../data",
+				dir: ".",
 			},
 			wantErr: false,
 		},
@@ -75,7 +75,7 @@ func TestService_ExportToFile(t *testing.T) {
 				favorites:     favorites,
 			},
 			args: args{
-				path: "../../data/accounts.dump",
+				path: "./accounts.dump",
 			},
 			wantErr: false,
 		},
